9fdc20c4737024d4ebb480ffa1e009eaf7a23ada
tekhuy <huyle20@hotmail.com>
2015-01-13 15:21:40 +0000

    adding_links_spec passed

--- spec/adding_links_spec.rb
+++ spec/adding_links_spec.rb
@@ -5,7 +5,7 @@ feature "User adds a new link" do
   scenario "when browsing the homepage" do
     expect(Link.count).to eq(0)
     visit '/'
-    add_link("http://makersacademy.com/", "Makers Academy")
+    add_link("http://www.makersacademy.com/", "Makers Academy")
     expect(Link.count).to eq(1)
     link = Link.first
     expect(link.url).to eq("http://www.makersacademy.com/")
