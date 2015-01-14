449cbbaf4eabf22e0c55b6d220e04d8782d98b48
Andy <andygnewman@gmail.com>
2015-01-14 16:35:29 +0000

    test for duplicated email, passed

--- models/user.rb
+++ models/user.rb
@@ -5,14 +5,14 @@ class User
   include DataMapper::Resource
 
   property :id, Serial
-  property :email, String
+  property :email, String, unique: true, message: "This email is already taken"
   property :password_digest, Text
-
+  
   attr_reader :password
   attr_accessor :password_confirmation
 
   validates_confirmation_of :password
-
+  
   def password=(password)
     @password = password
     self.password_digest = BCrypt::Password.create(password)
