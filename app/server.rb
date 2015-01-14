814bf5e0fee0c01d6e698b79c8d89063a2a999ad
Andy <andygnewman@gmail.com>
2015-01-14 14:05:15 +0000

    password confirmation validation test green

--- app/server.rb
+++ app/server.rb
@@ -47,12 +47,11 @@ class BookmarkManager < Sinatra::Base
   end
 
   post '/users' do
-    if params[:password] == params[:password_confirmation]
-      user = User.create(email: params[:email],
-        password: params[:password])
-      session[:user_id] = user.id
-    end
-      redirect to('/')
+    user = User.create(email: params[:email],
+      password: params[:password],
+      password_confirmation: params[:password_confirmation])
+    session[:user_id] = user.id
+    redirect to('/')
   end
 
 end
