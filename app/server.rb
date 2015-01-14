66ffd3d23e979b59a5d95c3509f8833cc3cc4410
Andy <andygnewman@gmail.com>
2015-01-14 17:40:27 +0000

    user sign in validation tests green

--- app/server.rb
+++ app/server.rb
@@ -51,4 +51,20 @@ class BookmarkManager < Sinatra::Base
     end
   end
 
+  get '/sessions/new' do
+    erb :"sessions/new"
+  end
+
+  post '/sessions' do
+    email, password = params[:email], params[:password]
+    user = User.authenticate(email, password)
+    if user
+      session[:user_id] = user.id
+      redirect to('/')
+    else
+      flash[:errors] = ["The email or password is incorrect"]
+      erb :"sessions/new"
+    end
+  end
+
 end
